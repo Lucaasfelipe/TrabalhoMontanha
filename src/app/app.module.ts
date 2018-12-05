7032afa1284b844c04fc7f2f8d88ef72b008020b
Gladson Santos <gladsonbruno16@gmail.com>
2018-12-04 22:27:24 -0200

    Login ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,22 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HOMEComponent } from './home/home.component';
 
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import { FormsModule } from '@angular/forms';
+
+import { APP_BASE_HREF } from '@angular/common';
+
+const firebaseAuth  = {
+  apiKey: "AIzaSyBS8gIOtlo0HgIdtvT9dFF4s_jpbkJ8ino",
+  authDomain: "trabmotanha.firebaseapp.com",
+  databaseURL: "https://trabmotanha.firebaseio.com",
+  projectId: "trabmotanha",
+  storageBucket: "trabmotanha.appspot.com",
+  messagingSenderId: "611786546158"
+};
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,9 +30,12 @@ import { HOMEComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseAuth),
+    AngularFireDatabaseModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: ''}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Injectable } from '@angular/core';
+import {AngularFireAuth} from 'angularfire2/auth';
+import { EventEmitter } from 'events';
+import {Router} from '@angular/router';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [AngularFireAuth]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email:any;
+  senha:any;
+
+  constructor(private fire: AngularFireAuth,private router:Router) { }
 
   ngOnInit() {
+  }
+
+  signInUser() {
+        this.fire.auth.signInWithEmailAndPassword(this.email, this.senha).then((user) => {
+
+          
+          this.router.navigate(['home']);
+        }) .catch(error => {
+          console.log('got an error',error);
+          alert('Email ou Senha incorreto!')
+        
+      });
+
   }
 
 }
